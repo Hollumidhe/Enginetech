0a0ee139403100fcb8715b6b42db175b5cc95d29
Olubimi Michael <Olubimimichael01@gmail.com>
2023-04-10 15:44:33 +0000

    routing

--- demo/app.js
+++ demo/app.js
@@ -3,19 +3,28 @@ const server =  http.createServer( (request,response) =>{
 
     response.writeHead(200, {"content-type": "text/html"});
 
+if(request.url === './get'){
+    response.write('You have gotten to GET page');
     if(request.method === "GET"){
         response.end("GET");
     }
-    if(request.method === "POST"){
-        response.end("POST");
-    }
-    if(request.method === "PUT"){
+}else if(request.url === './put'){
+    response.write('You have gotten to PUT page');
+     if(request.method === "PUT"){
         response.end("PUT");
     }
-    if(request.method === "DELETE"){
+}else if(request.url === './post'){
+    response.write('You have gotten to POST page');
+     if(request.method === "POST"){
+        response.end("POST");
+    }
+}else if(request.url === './delete'){
+    response.write('You have gotten to DELETE page');
+     if(request.method === "DELETE"){
         response.end("DELETE");
     }
-    
-    
-
+}else {
+    response.end('REQUEST NOT FOUND');
+}
+   response.end();
 }).listen(3000, ()=>console.log("http//localhost:3000"));
--- demo/apppp.js
+++ demo/apppp.js
@@ -41,3 +41,37 @@ const server =  http.createServer( (request,response) =>{
     
 
 }).listen(3000, ()=>console.log("http//localhost:3000"));
+
+
+
+////ROUTING ///////////////////////////////////////////////
+const http = require('http');
+const server =  http.createServer( (request,response) =>{
+
+    response.writeHead(200, {"content-type": "text/html"});
+
+if(request.url === './get'){
+    response.write('You have gotten to GET page');
+    if(request.method === "GET"){
+        response.end("GET");
+    }
+}else if(request.url === './put'){
+    response.write('You have gotten to PUT page');
+     if(request.method === "PUT"){
+        response.end("PUT");
+    }
+}else if(request.url === './post'){
+    response.write('You have gotten to POST page');
+     if(request.method === "POST"){
+        response.end("POST");
+    }
+}else if(request.url === './delete'){
+    response.write('You have gotten to DELETE page');
+     if(request.method === "DELETE"){
+        response.end("DELETE");
+    }
+}else {
+    response.end('REQUEST NOT FOUND');
+}
+   response.end();
+}).listen(3000, ()=>console.log("http//localhost:3000"));
