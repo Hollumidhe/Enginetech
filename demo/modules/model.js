2d43387073dfcd2aa7a42c96c3acbc584b3a8f77
Olubimi Michael <Olubimimichael01@gmail.com>
2023-04-10 12:35:43 +0000

    another lesson

--- demo/modules/model.js
+++ demo/modules/model.js
@@ -2,5 +2,11 @@
 
 //console.log(__dirname)
 //console.log(__filename)
+let name = ['olubimi', 'michael', 'olumide']
+let scores = [1000, 2000,3000]
+
+exports.scores = scores
+module.exports.name = name
+exports.data = [100, 200, 300]
 
 console.log(module)    
