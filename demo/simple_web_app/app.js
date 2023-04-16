3e9384559afdb372d90f53dff8ce3eb1d0265bf0
Olubimi Michael <Olubimimichael01@gmail.com>
2023-04-16 20:03:18 +0000

    the corecction on the simple node app

--- demo/simple_web_app/app.js
+++ demo/simple_web_app/app.js
@@ -1,22 +1,22 @@
 const http = require('http');
 const fs = require('fs');
-const dir   = './public';
-const port = process.env.PORT | 3000;
+const dir   = './public/';
+const port = process.env.PORT | 4000;
 
 //creating the server and routing
 // creating the server starts
-const server = http.createServer( (request, response) =>{
+const server = http.createServer( (req, res) =>{
 
     // routing the frontend here startssss
-    if(request.url === './'){
-        render(response, 'index.html');
-    }else if(request.url === '/about'){
-        render(response, 'about.html');
-    }else if(request.url === '/contact'){
-        render(response, 'contact.html');
+    if(req.url === '/'){
+        render(res, 'index.html');
+    }else if(req.url === '/about'){
+        render(res, 'about.html');
+    }else if(req.url === '/contact'){
+        render(res, 'contact.html');
     }else{
-        response.writeHead(404, {'content-type' : 'text/html'});
-        response.end('<h1> 404 file not found </h1>')
+        res.writeHead(404, {'Content-Type' : 'text/html'});
+        res.end('<h1> 404 file not found </h1>')
     }
      // routing the frontend here end
 
@@ -27,15 +27,15 @@ const server = http.createServer( (request, response) =>{
 
 // we will load the document on the front end here (or we will use the fs read file here)
 // startsssssssssssssss
-const render = (response, file) =>{
-    fs.readFile(dir+file, (error, data) =>{
+const render = (res, file) => {
+    fs.readFile(dir+file, (err, data) =>{
         //if error
-        if(error){
-            response.writeHead(404, {'content-type' : 'text/html'});
-        response.end('<h1> 404 file not found </h1>')
+        if(err){
+            res.writeHead(404, {'Content-Type' : 'text/html'});
+        res.end('<h1> 404 file not found </h1>')
         }
         // if no error
-        response.writeHead(200, {'content-type' : 'text/html'});
-        return response.end(data);
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        return res.end(data);
     });
 }
