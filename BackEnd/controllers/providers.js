e65e889f567ccae64b020b7c1d293ae5684bced0
Olubimi Michael <Olubimimichael01@gmail.com>
2023-06-17 20:17:13 +0000

    creating list

---
+++ BackEnd/controllers/providers.js
@@ -0,0 +1,6 @@
+const providers = require('../models/providers') 
+
+
+module.exports.list = function(req,res) {
+    res.render('providers/provider-list', { providers : providers} )   
+}
---
+++ BackEnd/models/providers.js
@@ -0,0 +1,63 @@
+var providers = [
+	{
+		"id": 414532,
+		"firstname": "Marylinda",
+		"lastname": "Beyir",
+		"position": "Managing director",
+		"company": {
+			"company_name": "Alpha court",
+			"address": "101 maryland way",
+			"address2": "",
+			"city": "Lagos",
+			"postal_code": "Marylinda",
+			"phone": "08078745487",
+			"email": "michaeloieuyt@gmail",
+			"description": "ersatiel Asynchronos Collaboration",
+			"tagline": "ersatiel Asynchronos Collaboration"
+
+		}
+	},
+
+
+	{
+		"id": 41998632,
+		"firstname": "Mhruuti",
+		"lastname": "kafyir",
+		"position": "Product director",
+		"company": {
+			"company_name": "Alpha Base",
+			"address": "101 maryland way",
+			"address2": "",
+			"city": "Lagos",
+			"postal_code": "Marylinda",
+			"phone": "08078745487",
+			"email": "michaeloieuyt@gmail",
+			"description": "ersatiel Asynchronos Collaboration",
+			"tagline": "ersatiel Asynchronos Collaboration"
+
+		}
+	},
+
+
+
+	{
+		"id": 32998632,
+		"firstname": "karruuti",
+		"lastname": "jayir",
+		"position": "Medical director",
+		"company": {
+			"company_name": "Tango Base",
+			"address": "101 maryland way",
+			"address2": "",
+			"city": "Lagos",
+			"postal_code": "Marylinda",
+			"phone": "08078745487",
+			"email": "michaeloieuyt@gmail",
+			"description": "ersatiel Asynchronos Collaboration",
+			"tagline": "ersatiel Asynchronos Collaboration"
+
+		}
+	},
+
+]
+module.exports = providers;
